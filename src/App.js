1d8fa3b251ec1b87587dbff6aa0e30743732c402
Vicky <vicky.plassais@gmail.com>
2023-11-29 11:35:51 +0100

    merge app.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-<<<<<<< HEAD
-import Greetings from './components/Greetings';
-import ItemsTopics from './components/ItemsTopics';
-import data from './assets/data'
 
-function App() {
-  console.log(data);
-  return (
-    <main className='bg-slate-900 min-h-screen text-white'>
-      <div className='max-w-7xl mx-auto grid md:grid-cols-3 justify-center items-center min-h-screen md:space-x-36 px-8 flex-wrap'>
-      <Greetings></Greetings>   
-      
-      <section className='w-full md:col-span-2'>
-        <ul className='w-full flex flex-col gap-5'>
-          {data.quizzes.map((item,index)=>(
-          <ItemsTopics key={index} 
-          title={item.title} 
-          logo={item.icon} ></ItemsTopics>
-          ))}
-        </ul>
-      </section>
-      </div>
-
-   
-    </main>
-=======
 import logo from './logo.svg';
 import './App.css';
-
+// commit
 function App() {
   return (
     <div className="App">
@@ -45,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
->>>>>>> 6ea6f36 (Initialize project using Create React App)
+
   );
 }
 
